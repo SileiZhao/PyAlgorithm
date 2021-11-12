054ea7ad7465aaa8ea4f960b7f7885f545a3afd5
SuperSilei <sileizhao@icloud.com>
2021-11-12 23:46:14 +0800

    update SelectionSort_1

--- SelectionSort.py
+++ SelectionSort.py
@@ -8,6 +8,19 @@ class Solution(object):
                 min_index = j if array[j] < array[min_index] else min_index
             array[i], array[min_index] = array[min_index], array[i]
         return array
+    def SelectionSort_1(self, array: [int]) -> [int]:
+        n = len(array)
+        for i in range(n):
+            min_index = i
+            max_index = n - 1
+            n = n - 1
+            for j in range(i+1,n+1):
+                if array[j] < array[min_index]:
+                    min_index = j
+                if array[j] > array[max_index]:
+                    max_index = j
+            array[min_index], array[i] = array[i], array[min_index] #将最小的放到本轮的最前面
+            array[max_index], array[n] = array[n], array[max_index] #将最大的放到本轮的最前面
 
 
 if __name__ == '__main__':
