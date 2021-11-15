84e6927d03752e1e05850bc6bef4b8eac0863760
SuperSilei <sileizhao@icloud.com>
2021-11-15 22:09:13 +0800

    Small bug.2

--- SelectionSort.py
+++ SelectionSort.py
@@ -8,13 +8,14 @@ class Solution(object):
                 min_index = j if array[j] < array[min_index] else min_index
             array[i], array[min_index] = array[min_index], array[i]
         return array
+
     def SelectionSort_1(self, array: [int]) -> [int]:
         n = len(array)
         for i in range(n):
             min_index = i
             max_index = n - 1
             n = n - 1
-            for j in range(i+1, n+1):
+            for j in range(i + 1, n + 1):
                 if array[j] < array[min_index]:
                     min_index = j
                 if array[j] > array[max_index]:
