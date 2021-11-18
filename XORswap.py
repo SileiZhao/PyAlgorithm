6da8d617d4a072d8360f2f0182a4f7b61c3055dd
SuperSilei <sileizhao@icloud.com>
2021-11-18 10:57:36 +0800

    supplement at the time of swap.

--- XORswap.py
+++ XORswap.py
@@ -1,4 +1,6 @@
 # how to swap two number with xor?
+# but, it does not work if i and j is a same position at a array,
+# or i and j is the same address at the memory.
 def swap(a: int, b: int) -> int:
     a = a ^ b
     b = a ^ b
